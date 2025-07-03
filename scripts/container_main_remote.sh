a98ebe8865d1680ad7773c38513fbcf4cd369672
vtaboga <vincent.taboga@mila.quebec>
2025-07-03 14:57:41 -0400

    update python version

--- scripts/container_main_remote.sh
+++ scripts/container_main_remote.sh
@@ -4,7 +4,7 @@
 set -e
 
 module load singularity
-module load python/3.10
+module load python/3.11
 
 # Get the repository root directory
 REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
